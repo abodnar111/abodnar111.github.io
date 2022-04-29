1500135526237355bc9f2bc6f8ebfb7469b4a158
abodnar111 <104648823+abodnar111@users.noreply.github.com>
2022-04-29 21:20:47 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.hamburger').addEventListener('click', function(){
+   document.querySelector('.hamburger').classList.toggle('hamburger__active');
+ })
+
+ document.querySelector('.hamburger').addEventListener('click', function(){
+   document.querySelector('.menu').classList.toggle('menu__active');
+ })
